42609a07b206bb0525a617db8e2983511e4b078d
Jan <jan.carius@gmx.de>
2020-10-09 16:08:07 +0200

    display request dictionary

--- Categorizing/tables.py
+++ Categorizing/tables.py
@@ -3,5 +3,14 @@ from flask_table import Table, Col
 
 
 class SupportItemTable(Table):
-    name = Col('Name')
-    topic = Col('Topic')
+    in_timestamp = Col('Time', attr_list=['input', 'timestamp'])
+    in_message = Col('Message', attr_list=['input', 'message'])
+    in_user_name = Col('User Name', attr_list=['input', 'user_name'])
+    in_contact_details = Col('User Contact', attr_list=[
+                             'input', 'contact_details'])
+    out_timestamp = Col('Time processed', attr_list=['output', 'timestamp'])
+    out_sentiment = Col('Sentiment', attr_list=['output', 'sentiment'])
+    out_assignee = Col('Assignee', attr_list=['output', 'assignee'])
+    out_answers = Col('Answers', attr_list=['output', 'answers'])
+    # name = Col('Name')
+    # topic = Col('Topic')
