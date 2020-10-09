2bca4ca44cc73b264e9f5d7c2faa1cfad79a48ac
Jonathan <jonathan.kuelz@chicken-technologies.com>
2020-10-09 15:35:05 +0200

    Parse Mails includes wordcount now

--- Categorizing/ParseMails.py
+++ Categorizing/ParseMails.py
@@ -6,6 +6,7 @@ requires xlrd package!
 from copy import deepcopy
 import numpy as np
 import pandas as pd
+import re
 
 DWB_XLXS = './../data/Mails.xlsx'
 
@@ -17,6 +18,7 @@ def main():
     print("{} Unique Categories in the data:\n".format(len(unique)), unique)
     dwf_dict = dwv_df.to_dict()
     # Do whatever you want with that
+    mail_wordcount = count_occurences(dwv_df['Mail'])
     return dwv_df, dwf_dict
 
 
@@ -35,5 +37,22 @@ def parse_xlsx(fpath):
     return raw
 
 
-if __name__== '__main__':
+def count_occurences(series):
+    """Dont use this, return will change!"""
+    words = {}
+    for row in series:
+        txt = re.split(' |\n|\\.', row)
+        # Split the words in the mail, delete punctiation and delete words that are actually only punctuation
+        # We ignore upper/lower case
+        txt = [t.strip().lower() for t in txt if not re.fullmatch('[ ,.]*', t)]
+        for word in txt:
+            if word in words:
+                words[word] += 1
+            else:
+                words[word] = 0
+    wordlist_sorted = sorted([(count, word) for word, count in words.items()], reverse=True)
+    return words, wordlist_sorted
+
+
+if __name__ == '__main__':
     main()
